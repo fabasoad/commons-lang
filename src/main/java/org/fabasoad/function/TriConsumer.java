d00d7fcf9dcc7badcd532442ab384be5af9ad85f
fabasoad <fabasoad@gmail.com>
2017-01-13 10:10:13 +0200

    Added TriConsumer class

---
+++ src/main/java/org/fabasoad/function/TriConsumer.java
@@ -0,0 +1,10 @@
+package org.fabasoad.function;
+
+/**
+ * @author Yevhen Fabizhevskyi
+ */
+@FunctionalInterface
+public interface TriConsumer<T1, T2, T3> {
+
+    void accept(T1 t1, T2 t2, T3 t3);
+}
