1e6b9081f8497f2ad8222d41ee41f7f143ae0fe8
fabasoad <fabasoad@gmail.com>
2017-03-15 09:18:30 +0200

    Added FourConsumer. Removing javax.ws.rs dependency. Removed PATCH annotation.

---
+++ src/main/java/org/fabasoad/function/FourConsumer.java
@@ -0,0 +1,10 @@
+package org.fabasoad.function;
+
+/**
+ * @author efabizhevsky
+ */
+@FunctionalInterface
+public interface FourConsumer<T1, T2, T3, T4> {
+
+    void accept(T1 t1, T2 t2, T3 t3, T4 t4);
+}
